31bb4f480e848434f88ac2c7fede9e30a702caca
Tiago Martinho <t.martinho@icloud.com>
2019-01-05 12:11:35 +0100

    extract extractBookCounts method

--- harryPotterKataTests/Domain/ShoppingCart.swift
+++ harryPotterKataTests/Domain/ShoppingCart.swift
@@ -7,12 +7,17 @@ class ShoppingCart {
     }
 
     func checkout() -> Double {
+        let booksCount = extractBookCounts()
+        let hasDiscount = booksCount.keys.count > 1
+        let multiplier = hasDiscount ? 0.95 : 1.0
+        return (books.reduce(0.0) { $0 + $1.price }) * multiplier
+    }
+
+    private func extractBookCounts() -> [Book: Int] {
         var booksCount = [Book: Int]()
         for book in books {
             booksCount[book] = (booksCount[book] ?? 0) + 1
         }
-        let hasDiscount = booksCount.keys.count > 1
-        let multiplier = hasDiscount ? 0.95 : 1.0
-        return (books.reduce(0.0) { $0 + $1.price }) * multiplier
+        return booksCount
     }
 }
