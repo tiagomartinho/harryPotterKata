bb225fb18a22a1087005cd6a9278ada04f79fd9a
Tiago Martinho <t.martinho@icloud.com>
2019-01-05 12:07:40 +0100

    Simplify books count

--- harryPotterKataTests/Domain/ShoppingCart.swift
+++ harryPotterKataTests/Domain/ShoppingCart.swift
@@ -9,11 +9,7 @@ class ShoppingCart {
     func checkout() -> Double {
         var booksCount = [Book: Int]()
         for book in books {
-            if let bookCount = booksCount[book] {
-                booksCount[book] = bookCount + 1
-            } else {
-                booksCount[book] = 1
-            }
+            booksCount[book] = (booksCount[book] ?? 0) + 1
         }
         let hasDiscount = booksCount.keys.count > 1
         let multiplier = hasDiscount ? 0.95 : 1.0
